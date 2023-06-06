a0eb8c138eff60707cac675e07752df2a9ae6f8d
gzow <the.zow@gmail.com>
2023-06-06 12:18:13 -0600

    Cambiando a PHP

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php include_once('views/head.php')?>
+<body>
+    <?php include_once('views/header.php') ?>
+    <main class="container">
+        <h1 class="title">Resumen del dia</h1>
+    </main>
+
+</body>
+<script src="assets/js/main.js"></script>
+</html>
---
+++ views/header.php
@@ -0,0 +1,16 @@
+<header class="header">
+        <div class="logo">
+            <!--<img class=logo__imagen src="assets/img/logo.png" alt="Logo Empanadas MAUGER"> -->
+            <h2 class="header__title">MAUGER</h2>
+        </div>
+        <div>
+            <div id="Menu" class="header__toggle">
+                <i id="Icon" class="gg-menu text-center m-center"></i>
+            </div>
+            <nav class="header__nav" id="Nav">
+                <a href="#" class="header__nav__link">Home</a>
+                <a href="#" class="header__nav__link">Regristro Cliente</a>
+                <a href="#" class="header__nav__link">Regristro Venta</a>
+            </nav>
+        </div>
+    </header>
