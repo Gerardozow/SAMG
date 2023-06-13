06c656a274dc1e550da4f43931979cd08d386969
gzow <the.zow@gmail.com>
2023-06-13 08:58:54 -0600

    Se termina el form general del registro del cliente

--- registro_cliente.php
+++ registro_cliente.php
@@ -7,45 +7,50 @@
         <h1 class="title upper text-center">Registro de Cliente</h1>
 
         <form action="" method="post" class="registro m-center">
-            <fieldset class="br-1 registro__grupo">
-                <legend>Datos del Cliente:</legend>
-                <div class="d-flex aling-center">
-                    <label for="cliente" class="mr-1 bold">Nombre del Negocio:</label>
-                    <input type="text" class="flex-1" id="cliente" name="cliente" placeholder="Ingresar Nombre del Negocio">
-                </div>               
-                <div class="d-flex aling-center mt-1">
-                    <label for="contacto" class="mr-1 bold">Contacto:</label>
-                    <input type="text" class="flex-1" id="contacto" name="contacto" placeholder="Ingresar Nombre del Contacto">
-                </div>
-                <div class="d-flex aling-center mt-1">
-                    <label for="telefono" class="mr-1 bold">Telefono:</label>
-                    <input type="tel" class="flex-1" id="telefono" name="telefono" placeholder="Numero de Telefono">
-                </div>
-                <div class="d-flex aling-center mt-1">
-                    <label for="email" class="mr-1 bold">Email:</label>
-                    <input type="email" class="flex-1" id="email" name="email" placeholder="Email">
-                </div>
+            <fieldset class="registro__grupo">
+                <legend>Datos Generales</legend>
+
+                <label for="nombre-negocio" class="registro__label">Nombre del Negocio:</label>
+                <input type="text" id="nombre-negocio" name="nombre-negocio" class="registro__input" required>
+
+                <label for="nombre-contacto" class="registro__label">Nombre de Contacto:</label>
+                <input type="text" id="nombre-contacto" name="nombre-contacto" class="registro__input"  required>
+
+                <label for="telefono-contacto" class="registro__label">Teléfono del Contacto:</label>
+                <input type="tel" id="telefono-contacto" name="telefono-contacto" class="registro__input"  required>
+
+                <label for="email-contacto" class="registro__label">Email del Contacto:</label>
+                <input type="email" id="email-contacto" name="email-contacto" class="registro__input"  required>
+
             </fieldset>
-            <fieldset class="mt-1 br-1">
-                <legend>Direccion del Negocio:</legend>
-                <div class="d-flex aling-center">
-                    <label for="calle" class="mr-1 bold">Calle:</label>
-                    <input type="text" class="flex-1" id="calle" name="calle" placeholder="">
-                </div>               
-                <div class="d-flex aling-center mt-1">
-                    <label for="numero" class="mr-1 bold">Numero:</label>
-                    <input type="text" class="flex-1" id="numero" name="numero" placeholder="">
-                </div>
-                <div class="d-flex aling-center mt-1">
-                    <label for="colonia" class="mr-1 bold">Colonia:</label>
-                    <input type="text" class="flex-1" id="colonia" name="colonia" placeholder="">
-                </div>
-                <div class="d-flex aling-center mt-1">
-                    <label for="localidad" class="mr-1 bold">Localidad:</label>
-                    <input type="text" class="flex-1" id="localidad" name="localidad" placeholder="">
-                </div>
+            <fieldset class="registro__grupo">
+                <legend>Direccion</legend>
+
+                <label for="calle" class="registro__label">Calle:</label>
+                <input type="text" id="calle" name="calle" class="registro__input"  required>
+
+                <label for="numero" class="registro__label">Número:</label>
+                <input type="text" id="numero" name="numero" class="registro__input"  required>
+                
+                <label for="colonia" class="registro__label">Colonia:</label>
+                <input type="text" id="colonia" name="colonia" class="registro__input"  required>
+
+                <label for="cp" class="registro__label">Código Postal:</label>
+                <input type="text" id="cp" name="cp" class="registro__input"  required>
+
+                <label for="localidad" class="registro__label">Localidad:</label>
+                <input type="text" id="localidad" name="localidad" class="registro__input"  required>
+
+                <label for="estado" class="registro__label">Estado:</label>
+                <input type="text" id="estado" name="estado" class="registro__input"  required>
+
+
             </fieldset>
-            <input type="submit" value="Enviar">
+            
+
+            
+
+            <input type="submit" value="Enviar" class="block w100 btn-submit mt-1">
         </form>
     </main>
 
